ab7f6187b70081d34d53a5d769b59d9c9fdcf84d
Alexander Zharyokhin <azharyokhin@intelliants.com>
2022-01-23 19:49:12 +0300

    Validator

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Validator
+## Usage
+
+```Validator::is_not_looged_in()``` check user on not login. Return true/false
+
+```Validator::is_admin()``` check user having admin role. Return true/false
+
+```Validator::isEmail($email)``` check check email using filter_var with FILTER_VALIDATE_EMAIL. $email - string. Return true/false
+
+```Validator::requiredFields($requiredFields)```. Check required fields on empty value. Return true/false and set message if field is empty. $requiredFields is array of required fields
+
+
---
+++ Validator.php
@@ -0,0 +1,49 @@
+<?php
+
+class Validator
+{
+    /**
+     * @return bool
+     */
+
+    public static function is_not_looged_in()
+    {
+        return empty($_SESSION['user']);
+    }
+
+    /**
+     * @return bool
+     */
+
+    public static function is_admin() {
+        return isset($_SESSION['user']['role']) &&  $_SESSION['user']['role'] == 'admin';
+    }
+
+    /**
+     * @param $email
+     * @return bool
+     */
+
+    public static function isEmail(string $email)
+    {
+        return filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+
+    /**
+     * @param array $required_fields
+     * @return bool
+     */
+
+    public static function requiredFields(array $required_fields) {
+        $error = false;
+
+        foreach ($required_fields as $field) {
+            if (empty($_POST[$field])) {
+                Flash::setMessage("Поле $field должно быть заполнено");
+                $error = true;
+            }
+        }
+
+        return $error;
+    }
+}
